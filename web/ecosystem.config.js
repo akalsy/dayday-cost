51680c9e935dc05d3a7bf2238500d58d7bc57ca8
akalsy <hermanyu666@gmail.com>
2022-03-12 16:08:06 +0800

    路径更改

--- web/ecosystem.config.js
+++ web/ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
         host: '1.116.118.236',
         ref: 'origin/master',
         repo: 'https://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@github.com/akalsy/dayday-cost.git',
-        path: '/workspace/dayday-cost',
+        path: '/workspace/dayday-cost/web',
         'post-deploy': 'git reset --hard && git checkout master && git pull && npm i --production=false && npm run build:release && pm2 startOrReload ecosystem.config.js', // -production=false 下载全量包
         env: {
           NODE_ENV: 'production'
